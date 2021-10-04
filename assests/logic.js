c6472d20f541196deba0c1e4ecc7c4ca800d50c7
Gavin Murphy <gavinhmurphy@gmail.com>
2021-10-03 19:31:54 -0700

    fucntion frame

--- assests/logic.js
+++ assests/logic.js
@@ -5,4 +5,17 @@
 // consolt weather docs for api filter info
 // populate bars with filtered information from weather api
 // push weather info to local storage
+// second page for five day? display current and options page one, five on next page after click
 // max five bars for queries
+// style
+
+const cityName = document.querySelector('city-name');
+const cityLocation = document.querySelector('city-location');
+// name to location, push location to local
+const timeCurrent = document.querySelector('time-current');
+const fiveDayForecast = [];
+const displayKey=  [];
+// add listner to submit button
+userFormEl.addEventListener('submit', formSubmitHandler);
+languageButtonsEl.addEventListener('click', buttonClickHandler);
+
