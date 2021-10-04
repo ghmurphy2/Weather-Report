829050f871771fc71d16d83fe97c7f6b48882571
Gavin Murphy <gavinhmurphy@gmail.com>
2021-10-03 19:01:20 -0700

    filler html and psuedo

---
+++ assests/logic.js
@@ -0,0 +1,8 @@
+//begin with framework for bars
+//search bar with input to popultate bars with info
+//begin with query to location api, console log to confirm connection with location api, city to numerical value
+//establish api output as query for weather api
+// consolt weather docs for api filter info
+// populate bars with filtered information from weather api
+// push weather info to local storage
+// max five bars for queries
