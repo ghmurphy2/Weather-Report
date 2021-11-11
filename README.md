382c7c3a823a7e46791e9a2c62ac1f14d12ec452
Gavin Murphy <gavinhmurphy@gmail.com>
2021-11-10 21:12:46 -0800

    refactoring report

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # Weather-Report
+​
+## Table of contents
+​
+- [Technologies](#Technologies)
+- [Deployment](#Deployment)
+- [Code Overview](#Code-Overview)
+- [Summary](#Summary)
+​
+​
+​
+![Image](assets/landingpage.png)
+​
+## Technologies
+​
+Project is created with:
+​
+- [Material-UI](https://material-ui.com/)
+- [JavaScript](https://www.javascript.com/)
+- [Bootstrap](https://getbootstrap.com/)
+​
+## Containers
+​
+![Image](assets/containers.png)
+​
+## Deployment
+​
+Download the repository
+​
+​
+## Code Overview
+​
+- Basic overview
+​
+## Summary
+​
+- This project is a mock-CRM for a start-up/small brewery company. 
+
+​
+## Authors
+​
+Gavin Murphy
+​
+## License
+​
+- Open Source
--- assests/logic.js
+++ assests/logic.js
@@ -9,11 +9,12 @@
 // max five bars for queries
 // style
 // name to location, push location to local
-$(document).ready(function(){
-const currentTemp = $('#current-temperature');
-const currentHumidity = $('#current-humidity');
-const currentWindSpeed = $('#current-wind-speed');
-const currentUvi = $('#current-uvi');
+initStorage();
+
+var startBtn = $('button');
+var reportContainer = $('#report-container');
+var apiKey = '7a2db2f596eb6b607d917c5d588554f0';
+var currentTime = moment();
 
 
 $('#search-weather-btn').on('click', function(e){
@@ -24,6 +25,17 @@ $('#search-weather-btn').on('click', function(e){
     searchWeather(
     addWeatherData(temp))
 })
+function location(city) {
+    let lat, lon;
+    url = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=7a2db2f596eb6b607d917c5d588554f0&units=imperial";
+    fetch(url).then(response => response.json()).then(function(data) {
+        lat = data[0].lat;
+        lon = data[0].lon;
+        populateCurrentWeatherTitle(data[0].name + ", " + data[0].country);
+        getOneCallData(lat, lon);
+    });
+    return {"lat": lat, "lon":lon};
+}
 
 function addWeatherData(temp, humidity, windspeed, uvi){
     currentTemp.append("temp");
@@ -36,7 +48,7 @@ function addWeatherData(temp, humidity, windspeed, uvi){
 
 function searchWeather(city){
     // consult api with name   
-    let queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7a2db2f596eb6b607d917c5d588554f0&units=imperial`
+    let queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=` + city + `&appid=7a2db2f596eb6b607d917c5d588554f0&units=imperial`
         fetch(queryUrl).then(response => response.json()).then(function(data) {
             console.log(data)
             
@@ -44,3 +56,21 @@ function searchWeather(city){
         })
     }
 });
+// call in data
+function populateCurrentWeatherData(temp, humidity, windspeed, uvi) {
+    $("#current-temperature").text(temp);
+    $("#current-humidity").text(humidity);
+    $("#current-wind-speed").text(windspeed + " mph");
+
+    function popCurrentName(city) {
+        $("#current-city").text(city + "has a temperature forecast of");
+    }
+    
+    function populateForecastWeather(forecast) {
+        let cards = forecastContainer.children();
+        for(let i=0; i<cards.length; i++) {
+            $(cards[i]).find("h3.day").text(moment(now).add(i+1, "d").format("dddd"));
+            $(cards[i]).find("span.forecast-temperature").text(forecast[i].temp.day);
+            $(cards[i]).find("span.forecast-humidity").text(forecast[i].humidity);
+            $(cards[i]).find("img.forecast-uvi").attr("src", "https://openweathermap.org/img/wn/" + forecast[i].weather[0].icon + ".png");
+        }
