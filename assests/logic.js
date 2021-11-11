57764ff807405c3a9c33cb6c19b16b63bdbd54cd
Gavin Murphy <gavinhmurphy@gmail.com>
2021-11-10 21:23:59 -0800

    new functions for populate

--- assests/logic.js
+++ assests/logic.js
@@ -16,61 +16,43 @@ var reportContainer = $('#report-container');
 var apiKey = '7a2db2f596eb6b607d917c5d588554f0';
 var currentTime = moment();
 
+updateHistoryDisplay( JSON.parse( localStorage.getItem("weatherDashboardHistory") ) );
+search("Seattle");
 
-$('#search-weather-btn').on('click', function(e){
-    e.preventDefault()
-    console.log("click")
-    let city = $("#city-name").val();
-    console.log(`city:${city}`)
-    searchWeather(
-    addWeatherData(temp))
-})
+startBtn.click( function() {
+    let city = $("input").val();
+    addHistory(city);
+    search(city);
+} );
+
+
+
 function location(city) {
     let lat, lon;
-    url = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=7a2db2f596eb6b607d917c5d588554f0&units=imperial";
+    url = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + '&limit=1&appid=' + apiKey;
+    // api working
     fetch(url).then(response => response.json()).then(function(data) {
         lat = data[0].lat;
         lon = data[0].lon;
-        populateCurrentWeatherTitle(data[0].name + ", " + data[0].country);
+        weatherTitle(data[0].name + ", " + data[0].country);
         getOneCallData(lat, lon);
     });
     return {"lat": lat, "lon":lon};
 }
 
 function addWeatherData(temp, humidity, windspeed, uvi){
-    currentTemp.append("temp");
-    currentHumidity.innerHtml(humidity);
-    $("#current-wind-speed").innerHtml(windspeed + " mph");
-    $("#current-uvi").innerHtml(uvi);
+    $('#current-temperature').text(temp)
+    $('#current-humidity').text(humidity)
+    $('#current-windspeed').text(windspeed)
+    $('#current-uvi').text(uvi)
+}
 
+function weatherTitle(city){
+    $('#current-city').text(city + "currently: ")
 
 }
 
-function searchWeather(city){
-    // consult api with name   
-    let queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=` + city + `&appid=7a2db2f596eb6b607d917c5d588554f0&units=imperial`
-        fetch(queryUrl).then(response => response.json()).then(function(data) {
-            console.log(data)
-            
-            // check
-        })
-    }
-});
-// call in data
-function populateCurrentWeatherData(temp, humidity, windspeed, uvi) {
-    $("#current-temperature").text(temp);
-    $("#current-humidity").text(humidity);
-    $("#current-wind-speed").text(windspeed + " mph");
 
-    function popCurrentName(city) {
-        $("#current-city").text(city + "has a temperature forecast of");
-    }
-    
-    function populateForecastWeather(forecast) {
-        let cards = forecastContainer.children();
-        for(let i=0; i<cards.length; i++) {
-            $(cards[i]).find("h3.day").text(moment(now).add(i+1, "d").format("dddd"));
-            $(cards[i]).find("span.forecast-temperature").text(forecast[i].temp.day);
-            $(cards[i]).find("span.forecast-humidity").text(forecast[i].humidity);
-            $(cards[i]).find("img.forecast-uvi").attr("src", "https://openweathermap.org/img/wn/" + forecast[i].weather[0].icon + ".png");
-        }
+
+
+
